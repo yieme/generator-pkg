f77f63ddef886dfb7790a4ea1a25255e57be6674
yieme <yieme@cormea.com>
2015-05-05 14:21:23 -0600

    expand default cli

--- app/templates/modules/cli.js
+++ app/templates/modules/cli.js
@@ -6,8 +6,23 @@
  *  @param      {stream} stdin  - Standard input
  *  @return     {stream} stdout - Standard output
  */ 'use strict';
-var stdin = require('get-stdin')
+var convar  = require('convar')
 var <%= moduleVarName %> = require('../index.js')
+
+function help() {
+  var pkg = require('../package.json')
+  console.error(pkg.description, '- v' + pkg.version)
+  console.error('Usage:', pkg.name, '--param value')
+  process.exit(1)
+}
+
+var param = convar('param')
+if (!param) help()
+
+
+
+// standard in template
+var stdin   = require('get-stdin')
 
 stdin(function (data) {
   if (data) {
